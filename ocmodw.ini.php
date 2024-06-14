b60e86ecd61f37ea1fab286b938b6de5ed9bb522
akaunderr <akaunderr@gmail.com>
2024-06-14 10:59:43 +0300

    updated module wrapper

---
+++ ocmodw.ini.php
@@ -0,0 +1,22 @@
+<?php
+
+define('VERSION', '3.1.2');
+define('YEAR', '2019');
+
+define('AUTHOR', 'Andrii Burkatskyi');
+define('EMAIL', 'ocmod.space@gmail.com');
+
+define('MODLINK0', 'https://www.ocmod.space/equal-sign');
+define('MODLINK1', 'https://www.ocmod.space/equal-sign--journal');
+define('MODLINK2', 'https://www.ocmod.space/equal-sign--poco');
+
+define('OCMLINK0', 'https://www.opencart.com/index.php?route=marketplace/extension/info&extension_id=34383');
+define('OCMLINK1', 'https://www.opencart.com/index.php?route=marketplace/extension/info&extension_id=38532');
+define('OCMLINK2', 'https://www.opencart.com/index.php?route=marketplace/extension/info&extension_id=45235');
+
+define('GITLINK', 'https://github.com/ocmod-space/ocmod-equal-sign');
+define('XMLLINK', 'https://www.opencart.com/index.php?route=marketplace/extension&amp;filter_member=ocmod.space');
+
+define('LICENSE0', 'https://raw.githubusercontent.com/ocmod-space/ocmod-equal-sign/main/LICENSE.txt');
+define('LICENSE1', 'https://raw.githubusercontent.com/ocmod-space/ocmod-equal-sign/main/addons/journal/EULA.txt');
+define('LICENSE2', 'https://raw.githubusercontent.com/ocmod-space/ocmod-equal-sign/main/addons/poco/EULA.txt');
